ea26d2a2e3362a1f76eb108042f7de9cb764dff6
송찬영 <52197226+payo03@users.noreply.github.com>
2021-07-30 09:48:38 +0900

    로그인창 바로이동 - local확인완료

--- src/main/java/petProject/service/impl/email/MemberPasswordEmailSendServiceImpl.java
+++ src/main/java/petProject/service/impl/email/MemberPasswordEmailSendServiceImpl.java
@@ -23,25 +23,25 @@ import petProject.vo.dto.Member;
 @Service("memberPasswordEmailSendService")
 @Transactional
 public class MemberPasswordEmailSendServiceImpl implements MemberPasswordEmailSendService {
-   @Value("${mail.smtp.url.server}")
-   private String url;
+//	@Value("${mail.smtp.url.local}")
+	@Value("${mail.smtp.url.server}")
+	private String url;
 
-   @Resource(name = "mailSendService")
-   MailSendService mailSendService;
+	@Resource(name = "mailSendService")
+	MailSendService mailSendService;
 
-   @Override
-   public boolean sendPassword(Member member, String tempPassword, HttpServletRequest request,
-         boolean isHtml) throws Exception {
-      String mailHead = null;
-      String mailContent = null;
+	@Override
+	public boolean sendPassword(Member member, String tempPassword, HttpServletRequest request, boolean isHtml)
+			throws Exception {
+		String mailHead = null;
+		String mailContent = null;
 
-      mailHead = "<h1>[ANION] 임시 비밀번호 발급</h1><br><p>임시 비밀번호가 발급되었습니다. 로그인후 변경해주세요!</p><br>";
-      mailContent = tempPassword + "<br><a href='" + url + request.getContextPath() + "/login"
-            + "'target='_blank'>[ANION]</a>";
+		mailHead = "<h1>[ANION] 임시 비밀번호 발급</h1><br><p>임시 비밀번호가 발급되었습니다. 로그인후 변경해주세요!</p><br>";
+		mailContent = tempPassword + "<br><a href='" + url + request.getContextPath() + "/login"
+				+ "'target='_blank'>[ANION]</a>";
 
-      mailSendService.sendMail(member.getMemberId(), member.getMemberName(), mailHead, mailContent,
-            request, isHtml);
-      return true;
-   }
+		mailSendService.sendMail(member.getMemberId(), member.getMemberName(), mailHead, mailContent, request, isHtml);
+		return true;
+	}
 
 }
