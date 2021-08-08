ffbbb03470632ebefec01f0fe39ee7dd229988ec
송찬영 <52197226+payo03@users.noreply.github.com>
2021-08-08 13:02:13 +0900

    테스트 완료(withdrawal 기능 안되는 오류 해결해야함)

--- src/main/java/petProject/service/impl/member/MemberValidServiceImpl.java
+++ src/main/java/petProject/service/impl/member/MemberValidServiceImpl.java
@@ -27,20 +27,20 @@ public class MemberValidServiceImpl implements MemberValidService {
 
 	@Autowired
 	MemberDAO memberDAO;
-	
+
 	@Autowired
 	MemberWithdrawalDAO memberWithdrawalDAO;
 
-	// 한시간마다 memberAuthStatusValid메서드를 실행 : 이메일 인증을 하지 않은 사용자는 자동으로 삭제됨
-	@Override
-	@Scheduled(fixedDelay = 1000 * 60 * 60)
+	// 하루마다 memberAuthStatusValid메서드를 실행 : 이메일 인증을 하지 않은 사용자는 자동으로 삭제됨
+	@Override			//	1sec   1m	1h	 1d
+	@Scheduled(fixedDelay = 1000 * 60 * 60 * 24)
 	public void memberAuthStatusValid() {
 		memberDAO.memberAuthStatusValid();
 	}
 
-	// 한시간마다 memberWithdrawal메서드를 실행 : 7일동안 회원탈퇴를 신청한 회원은 기존아이디 + memberNumber로 변경됨
-	@Override
-	@Scheduled(fixedDelay = 1000 * 60 * 60)
+	// 하루마다 memberWithdrawal메서드를 실행 : 7일동안 회원탈퇴를 신청한 회원은 기존아이디 + memberNumber로 변경됨
+	@Override			//	1sec   1m	1h	 1d
+	@Scheduled(fixedDelay = 1000 * 60 * 60 * 24)
 	public void memberWithdrawal() {
 		List<Integer> memberNumberList = memberWithdrawalDAO.selectMemberNumberByDate();
 		for (Integer memberNumber : memberNumberList) {
